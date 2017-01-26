09f86433769f53873728129f78913392e8f37e59
1dam.m <1dam.m@PC112.cenecmalaga.net>
2017-01-26 12:11:03 +0100

    Ejercicio 31 listo

---
+++ src/Relacion_Tema5_04/Ejercicio_31.java
@@ -0,0 +1,35 @@
+package Relacion_Tema5_04;
+
+public class Ejercicio_31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] numeros = {1,2,3,4};
+		int [] numeros2 = {5,6,7,8};
+		int[]numcon = Concatenacion(numeros, numeros2);
+		
+		for(int i: numcon){
+			System.out.print(i + "\t");
+		}
+	}
+	
+	public static int[]Concatenacion(int[]array, int[]array2){
+		int []arraycon = new int[array.length + array2.length];
+		int posicion = 0;
+		
+		for(int i=0; i < array.length; i++){
+			arraycon[i] = array[i];
+			posicion = i;
+		}
+		
+		posicion++;
+		
+		for(int i=0; i < array2.length; i++){
+			arraycon[posicion] = array2[i];
+			posicion++;
+		}
+		
+		return arraycon;
+	}
+
+}
